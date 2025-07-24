f3817dd9dd5cbe85633c38a779a7bb11de64ac20
taeeun98 <xodms6360@naver.com>
2025-07-24 10:07:31 +0900

    [Feat] auth api, 로그인 component, 로그인 provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 // import { useState } from 'react'
 // import reactLogo from '@/assets/react.svg'
 // import viteLogo from '/vite.svg'
-import '@/App.css'
-import ApiTestPage from './Page/ApiTestPage'
+import "@/App.css";
+import ApiTestPage from "./Page/ApiTestPage";
+import LoginModal from "./components/LoginModal";
+import { LoginModalProvider } from "./context/LoginModalContext";
 
 function App() {
-
   return (
     <>
-      <ApiTestPage/>
+      <LoginModalProvider>
+        <ApiTestPage />
+        <LoginModal />
+      </LoginModalProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Page/ApiTestPage.tsx
+++ src/Page/ApiTestPage.tsx
@@ -1,10 +1,19 @@
-import ApiTest from "@/components/ApiTest";
+// import ApiTest from "@/components/ApiTest";
+
+import { useLoginModal } from "@/context/LoginModalContext";
+
 
 function ApiTestPage() {
+  
+  const { openLogin } = useLoginModal();
+  const handleClick = () => {
+    openLogin();
+  };
+
   return (
     <>
-      <ApiTest />
-      <div>ApiTestPage</div>
+    <div>ApiTestPage</div>
+      <button type="button" onClick={handleClick}>로그인</button>
     </>
   );
 }
