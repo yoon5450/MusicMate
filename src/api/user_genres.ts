42c825775c7b47500ca8a1e7b50f6d93851d3311
seungdev <trsemaris@gmail.com>
2025-07-24 16:06:10 +0900

    [Feat] user_genres api 추가

--- src/api/user_genres.ts
+++ src/api/user_genres.ts
@@ -1,2 +1,33 @@
 
 // TODO: VIEW 생성해서 연결된 장르 이름과 함께 조회
+/**
+ * @description 현재 로그인한 사용자의 선호 장르 코드 목록을 가져옵니다.
+ * @returns Promise<number[] | null> 선호장르배열, 실패 시 null
+ */
+import supabase from "@/utils/supabase";
+import type { Tables } from "@/@types/database.types";
+import errorHandler from "@/error/supabaseErrorHandler";
+
+export type UserGenreType = Tables<"user_genres">;
+
+
+export const getUserGenre = async ():Promise<number[] | null> => {
+  const {data : {user}} = await supabase.auth.getUser(); //현재 로그인한 사용자 정보 가져오기
+
+  if(!user){
+    return [];
+  }
+
+  const {data,error} = await supabase
+  .from("user_genres")
+  .select("genre_code")
+  .eq("user_id", user.id);
+
+  if(error){
+    errorHandler(error, "getUserGenre");
+    return null;
+  }
+
+  return data.map((item) => item.genre_code); //장르코드를 숫자 배열로 map 한 후 리턴하기
+}
+
