0c8b388313d21cde6b9af440934f4c6b899e43f9
yoon5450 <dbseodnd356@naver.com>
2025-07-24 12:24:37 +0900

    [Fix] 스크롤 영역 높이 계산 제대로 못하는 오류 수정

--- src/pages/Channel/index.tsx
+++ src/pages/Channel/index.tsx
@@ -10,7 +10,7 @@ function Channel() {
     <>
       <div className={S.contentArea}>
         <div>{`${id} 채널입니다.`}</div>
-        <div className={S.test}>`${id.repeat(300)}`</div>
+        {/* <div className={S.test}>`${id.repeat(300)}`</div> */}
       </div>
       <InputFeed />
     </>
