ecfd857f58303b1ccdad821908de7079860d3b99
taeeun98 <xodms6360@naver.com>
2025-07-24 14:23:19 +0900

    [Feat] 로그아웃 예시 버튼 추가

--- src/Page/ApiTestPage.tsx
+++ src/Page/ApiTestPage.tsx
@@ -1,20 +1,29 @@
 import ApiInsertTest from "@/test/ApiInsertTest";
 // import ApiTest from "@/components/ApiTest";
 import { useLoginModal } from "@/context/LoginModalContext";
+import { signOut } from "@/api/auth";
 
 
 function ApiTestPage() {
   
   const { openLogin } = useLoginModal();
-  const handleClick = () => {
+
+  const handleLoginModal = () => {
+    // 로그인 모달창 열기
     openLogin();
+  };
+
+  const handleLogout = () => {
+    // 로그아웃 하기
+    signOut();
   };
 
   return (
     <>
       <ApiInsertTest />
       <div>ApiTestPage</div>
-      <button type="button" onClick={handleClick}>로그인</button>
+      <button type="button" onClick={handleLoginModal}>모달창열기</button>
+      <button type="button" onClick={handleLogout}>로그아웃</button>
     </>
   );
 }
