b41f8da4132b612eafc7d154280dd10ddd7fba64
sujin209 <20201036@sungshin.ac.kr>
2025-08-01 10:03:08 +0900

    [Feat] 피드 좋아요 채널가입자만 가능하도록 구현

--- src/pages/Channel/index.tsx
+++ src/pages/Channel/index.tsx
@@ -3,7 +3,11 @@ import InputFeed from "./components/InputFeed";
 import S from "./Channel.module.css";
 import ChannelFeedMessage from "./components/ChannelFeedMessage";
 import { useEffect, useState } from "react";
-import { getFeedsWithAllByChannelId, getLikesByUserId } from "@/api";
+import {
+  checkUserInChannels,
+  getFeedsWithAllByChannelId,
+  getLikesByUserId,
+} from "@/api";
 import type { Tables } from "@/@types/database.types";
 import { getAvatarUrlPreview } from "@/api/user_avatar";
 import DetailFeeds from "./components/DetailFeeds";
@@ -35,6 +39,7 @@ function Channel() {
     Tables<"get_replies_with_user">[] | null
   >(null);
   const [updateReplies, setUpdateReplies] = useState<number>(Date.now);
+  const [isMember, setIsMember] = useState<boolean | null>(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -62,6 +67,17 @@ function Channel() {
     fetchData();
   }, [id, user, lastUpdatedAt]);
 
+  useEffect(() => {
+    async function check() {
+      if (id && user) {
+        const flag = await checkUserInChannels(id, user?.id);
+        setIsMember(flag);
+      }
+    }
+    check();
+    console.log(isMember);
+  }, [id, user]);
+
   // 선택된피드 바뀔때마다 해당 피드의 댓글 가져오기
   useEffect(() => {
     if (!selectedFeed?.feed_id) return;
@@ -85,7 +101,14 @@ function Channel() {
   };
 
   const onToggleLike = async (feedId: string) => {
-    if (!user) return;
+    if (!user) {
+      alert("피드에 좋아요를 누르려면 로그인해야 합니다.");
+      return;
+    }
+    if (!isMember) {
+      alert("피드에 좋아요를 누르려면 멤버여야 합니다.");
+      return;
+    }
     const result = await handleToggleLike(user.id, feedId, userLikes, feedData);
     if (!result) return;
     const { newUserLikes, newFeedData } = result;
