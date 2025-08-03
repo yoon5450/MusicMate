b9b08c0f3192cb9d0c4ead618d33d9de15e59860
seungdev <trsemaris@gmail.com>
2025-08-03 17:18:35 +0900

    [Feat] 인기 게시글 보기

--- src/pages/Main/components/FeedList.tsx
+++ src/pages/Main/components/FeedList.tsx
@@ -1,13 +1,17 @@
+/**
+ * 인기 게시글을 보여주고 각 게시글을 클릭 시 해당 채널로 이동합니다.
+ */
 import type { Tables } from "@/@types/database.types";
 import S from "./style.FeedList.module.css";
 import { useEffect, useState } from "react";
 import { getPopularFeeds } from "@/api";
+import ChannelLink from "@/components/common/Link/ChannelLink";
+import heartFilledWhite from "@/assets/heart_filled_white.svg";
 
 
 function FeedList() {
   const [feeds,setFeeds] = useState<Tables<"get_feeds_with_user_and_likes">[]>([]);
   
-
   useEffect(()=>{
     const fetchFeeds = async()=>{
       const data = await getPopularFeeds();
@@ -19,7 +23,27 @@ function FeedList() {
   return (
     <>
       <h2 className={S.title}>인기 게시글</h2>
-     
+      <div className={S.feedGrid}>
+        {feeds?.slice(0,6).map((feed)=>(
+          <div
+          key={feed.feed_id}
+          className={S.feedItem}
+          >
+            <ChannelLink channelId={feed.channel_id!} feedId={feed.feed_id!}>
+              <div className={S.postContent}>
+                <p className={S.preview}>{feed.content?.slice(0,40)}</p>
+              </div>
+            </ChannelLink>
+
+            <div className={S.likeContainer}>
+              <img 
+                style={{ width: "16px", height: "16px" }}
+                src={heartFilledWhite} alt="좋아요" />
+              <span className={S.likeCount}>{feed.like_count ?? 0}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
