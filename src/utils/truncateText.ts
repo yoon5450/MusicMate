ddcd9da08ef340e937847eb52cfe5474ac9649cf
sujin209 <20201036@sungshin.ac.kr>
2025-08-06 15:18:25 +0900

    [Design] 피드 이미지 패딩 삭제, 인기게시물 truncate 적용

--- src/utils/truncateText.ts
+++ src/utils/truncateText.ts
@@ -1,4 +1,4 @@
 export function truncateText(text: string, limit: number = 100): string {
-  if (text.length > limit) return text.slice(0, limit) + "...";
+  if (text.length > limit) return text.slice(0, limit) + " . . .";
   return text;
 }
